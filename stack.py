c77f459e60c6a9acf1ff6cffcedcbb5ea716ade7
major <echolabstech@gmail.com>
2017-02-13 18:09:01 -0500

    reimplement stack push using list methods and array

--- stack.py
+++ stack.py
@@ -1,14 +1,13 @@
 # Major Sapp III 02/13/2017
 def push(array, value):
-	swap = [None]*(len(array)+1)
-	if len(array) > 0:
-		for k,v in enumerate(array):
-			swap[k+1] = v
-	swap[0] = value
+	swap = []
+	for k,v in enumerate(array):
+		swap.insert(k+1, v)
+	swap.insert(0, value)
 	return swap
 
 if __name__ == "__main__":
-	array = [None]
+	array = []
 	print("before:",array)
 	array = push(array, "first")
 	array = push(array, "second")
