455a09e187d60017d5c0eba38aaae49a4847e847
major <echolabstech@gmail.com>
2017-02-13 18:29:53 -0500

    base stack pop implementation using array

--- stack.py
+++ stack.py
@@ -6,6 +6,15 @@ def push(array, value):
 	swap.insert(0, value)
 	return swap
 
+def pop(array):
+	value = array[0]
+	swap = []
+	i=0
+	while i < (len(array)):
+		swap.insert(i, array[i+1])
+		i += i + 1
+	return (value, swap)
+
 if __name__ == "__main__":
 	array = []
 	print("before:",array)
@@ -13,3 +22,6 @@ if __name__ == "__main__":
 	array = push(array, "second")
 	array = push(array, "third")
 	print("after:",array)
+	value, array = pop(array)
+	print("pop {}".format(value))
+	print("after pop:",array)
