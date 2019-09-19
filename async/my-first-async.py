e951a1de3e2f4aa8ac45a861bb3931060208b5fa
major <echolabstech@gmail.com>
2019-09-20 05:44:01 +0800

    use coroutine to search google books for star wars books

--- async/my-first-async.py
+++ async/my-first-async.py
@@ -8,11 +8,26 @@ URLS = {
     'search': 'https://www.googleapis.com/books/v1/volumes?q=',
 }
 
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
 # search_term - string of search terms
-def search_books_for(search_term):
+async def search_books_for(search_term):
     query_param = search_term.lower().split(' ');
     query_param = '+'.join(query_param)
-    print(query_param);
+    async with aiohttp.ClientSession() as session:
+        url = URLS['search'] + search_term
+        html = await fetch(session, url)
+        print(html)
+
+async def main():
+    search_terms = ['star wars']
+    tasks = []
+    for search_term in search_terms:
+        tasks.append(asyncio.create_task(search_books_for(search_term)))
+    await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
-    search_books_for('star wars')
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
