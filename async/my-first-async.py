bbacd6cf1dd4fc23983df68678b4cc217d2f241f
major <echolabstech@gmail.com>
2019-09-21 05:20:34 +0800

    producer-consumer pattern to search and parse results concurrently

--- async/my-first-async.py
+++ async/my-first-async.py
@@ -18,34 +18,42 @@ URLS = {
     'search': 'https://www.googleapis.com/books/v1/volumes?q=',
 }
 
+def get_url(url, search_term=''):
+    return URLS[url] + search_term
+
 async def fetch(session, url):
     print(f'requesting {url}')
     async with session.get(url) as response:
-        return await response.text()
+        html = await response.text()
 
-# search_term - string of search terms
-async def search_books_for(search_term):
+        test_MOCK_RESPONSE_DELAY = random.randint(1, 3)
+        await asyncio.sleep(test_MOCK_RESPONSE_DELAY)
+
+        return html
+
+async def search_books_for(queue, search_term):
+    """
+    producer
+    queue - save fetch result
+    search_term - string of search terms
+    """
+
+    start = time.perf_counter()
     query_param = search_term.lower().split(' ');
     query_param = '+'.join(query_param)
     async with aiohttp.ClientSession() as session:
-        url = URLS['test'] + search_term
-        html = await fetch(session, url)
-        print(f'succesfully searched {search_term}')
+        url = get_url('search', search_term)
+        await queue.put((await fetch(session, url), url, start))
 
-async def produce_book_search_term(queue, search_term):
-    pass
-    # await new book search term
-    # await access to queue and put search request into queue
+async def handle_book_search_result(queue):
+    """
+    consumer
+    queue - retrieve request result from
+    """
 
-async def consume_book_search_term(queue):
-    pass
-    # await search request to be added to queue
-    # await request result and add result to queue
-
-async def handle_book_search_result(results):
-    pass
-    # await request result added to queue
-    # parse request result (i.e. print for now)
+    search_results, url, start = await queue.get()
+    stop = time.perf_counter()
+    print(f'recieved response from {url} in {stop - start:0.5f} seconds')
 
 def test_batch_search_terms():
     all_search_terms = ['star wars', 'pale blue dot', 'game of thrones', 'star trek', 
@@ -53,13 +61,13 @@ def test_batch_search_terms():
     return all_search_terms[:random.randint(1, 3)]
 
 async def main():
-    test_BATCH_DELAY = 1
+    queue = asyncio.Queue()
     while True:
-        tasks = []
         for search_term in test_batch_search_terms():
-            tasks.append(asyncio.create_task(search_books_for(search_term)))
-        await asyncio.gather(*tasks)
-        await asyncio.sleep(test_BATCH_DELAY)
+            asyncio.create_task(search_books_for(queue, search_term))
+            asyncio.create_task(handle_book_search_result(queue))
+        test_MOCK_RECEIVE_SEARCH_TERM_DELAY = random.randint(1, 3)
+        await asyncio.sleep(test_MOCK_RECEIVE_SEARCH_TERM_DELAY)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
