ac89638bc83c3f7a86d03569e83af43f784e38fe
major <echolabstech@gmail.com>
2019-09-22 05:25:33 +0800

    implement test_main_non_blocking

--- async/my_first_async.py
+++ async/my_first_async.py
@@ -1,75 +1,57 @@
-import aiohttp
+"""
+todo
+1. using the same server (googleapis) so only need one session. Move session out of
+search_books_for and into main
+"""
+from aiohttp import ClientSession
 import asyncio
-import time
-import random
 
 
 API_KEY = 'AIzaSyDSc4h-R_ByHJAr7wzPeqEHne9hkaMIa4w';
 UUID = '111456584073054736246';
 
-URLS = {
-    'test': 'http://localhost:8001/',
-    'search': 'https://www.googleapis.com/books/v1/volumes?q=',
-}
-
-
-def get_url(url, search_term=''):
-    return URLS[url] + search_term
-
-
-def test_batch_search_terms():
-    all_search_terms = ['star wars', 'pale blue dot', 'game of thrones', 'star trek',
-                        'Stargate'] * 2
-    return all_search_terms[:random.randint(1, 3)]
-
 
 async def fetch(session, url):
+    """
+    high level function to do http request
+    """
     print(f'requesting {url}')
     async with session.get(url) as response:
         html = await response.text()
-
-        test_MOCK_RESPONSE_DELAY = random.randint(1, 3)
-        await asyncio.sleep(test_MOCK_RESPONSE_DELAY)
-
         return html
 
 
-async def search_books_for(queue, search_term):
+async def search_books_for(queue, search_term, url=None):
     """
-    producer
-    queue - save fetch result
-    search_term - string of search terms
+    acts as a producer, initiating search and
+    saving request results in queue.
     """
-
-    start = time.perf_counter()
-    query_param = search_term.lower().split(' ');
-    query_param = '+'.join(query_param)
-    async with aiohttp.ClientSession() as session:
-        url = get_url('search', search_term)
-        await queue.put((await fetch(session, url), url, start))
+    async with ClientSession() as session:
+        query_param = search_term.lower().split(' ');
+        query_param = '+'.join(query_param)
+        url = url if url else 'https://www.googleapis.com/books/v1/volumes?q=' + query_param
+        await queue.put((await fetch(session, url), url))
 
 
 async def handle_book_search_result(queue):
     """
-    consumer
-    queue - retrieve request result from
+    acts as a consumer, retrieving request results from queue
     """
-
-    search_results, url, start = await queue.get()
-    stop = time.perf_counter()
-    print(f'recieved response from {url} in {stop - start:0.5f} seconds')
+    search_results, url = await queue.get()
 
 
-async def main():
+async def main(search_terms, url=None):
+    """
+    extract search terms from request, create and schedule
+    tasks to search terms and handle results
+    """
     queue = asyncio.Queue()
-    while True:
-        for search_term in test_batch_search_terms():
-            asyncio.create_task(search_books_for(queue, search_term))
-            asyncio.create_task(handle_book_search_result(queue))
-        test_MOCK_RECEIVE_SEARCH_TERM_DELAY = random.randint(1, 3)
-        await asyncio.sleep(test_MOCK_RECEIVE_SEARCH_TERM_DELAY)
+    tasks = []
+    for search_term in search_terms:
+        tasks.append(asyncio.create_task(search_books_for(queue, search_term, url)))
+        tasks.append(asyncio.create_task(handle_book_search_result(queue)))
+    return tasks
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    pass
--- async/test_my_first_async.py
+++ async/test_my_first_async.py
@@ -1,58 +1,121 @@
 import my_first_async
+import asyncio
+import pytest
+from unittest import TestCase
+from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
+from aiohttp import web
+import random
+from datetime import datetime
 
 
-def test_producer_non_blocking():
-    """
-    Producer A is awaiting a result, and passes control to the event loop.
-    """
-    assert 1 == 2
+class TestMyFirstAsync(AioHTTPTestCase, TestCase):
+    tasks = []
+    task_completion_times = {}
+    success_non_blocking = 'test success: main non-blocking'
+    failure_blocking = 'test failure: main is blocking'
+
+    def get_num_pending_tasks(self):
+        pending_tasks = 0
+        for task in self.tasks:
+            if not task.done():
+                pending_tasks = pending_tasks + 1
+        return pending_tasks
 
 
-
-@pytest.mark.skip(reason='not implemented')
-def test_producer_concurrent():
-    """
-    Producer B does work while Producer A awaits a result.
-    """
-    assert 1 == 2
+    async def webpage(self, request=None):
+        mock_response_delay = random.randint(0, 1)
+        await asyncio.sleep(mock_response_delay)
+        html = 'foobar'
+        return web.Response(text=html)
 
 
-@pytest.mark.skip(reason='not implemented')
-def test_consumer_non_blocking():
-    """
-    consumer A is awaiting a result, and passes control to the event loop.
-    """
-    assert 1 == 2
+    async def get_application(self):
+        """
+        Override the get_app method to return your application.
+        """
+        app = web.Application()
+        app.router.add_get('/', self.webpage)
+        return app
 
 
-@pytest.mark.skip(reason='not implemented')
-def test_consumer_concurrent():
-    """
-    consumer B does work while Producer A awaits a result.
-    """
-    assert 1 == 2
+    async def listen_requests(self, search_terms):
+        tasks = await my_first_async.main(search_terms, url='/')
+        await asyncio.sleep(1)
+        return tasks
 
 
-@pytest.mark.skip(reason='not implemented')
-def test_catches_404_page_not_found():
-    assert 1 == 2
+    async def await_pending_tasks(self):
+        """
+        holds the event loop open to complete running tasks
+        """
+        num_pending_tasks = self.get_num_pending_tasks()
+        await asyncio.sleep(num_pending_tasks)
 
 
-@pytest.mark.skip(reason='not implemented')
-def test_catches_400_bad_request():
-    assert 1 == 2
+    async def check_completition_times(self, targets):
+        """
+        determine if target tasks completed before other tasks
+        """
+        for target_task in targets:
+            target_completed_time = self.task_completion_times[target_task]
+            for task in self.tasks:
+                task_completed_time = self.task_completion_times[task]
+                if target_completed_time < task_completed_time:
+                    raise Exception(self.success_non_blocking)
+        raise Exception(self.failure_blocking)
 
 
-@pytest.mark.skip(reason='not implemented')
-def test_handle_302_redirect():
-    assert 1 == 2
+    # @pytest.mark.skip(reason='not implemented')
+    @unittest_run_loop
+    async def test_main_non_blocking(self):
+        """
+        Main can schedule tasks concurrently without blocking I/O
+        """
+        tasks = []
+        asyncio.set_event_loop(self.loop)
+        my_first_async.ClientSession.get = self.client.get
+        all_search_terms = ['star wars', 'pale blue dot', 'game of thrones', 'star trek',
+                            'Stargate'] * 2
+
+        search_terms = all_search_terms[:10]
+        self.tasks = self.tasks + await self.listen_requests(search_terms)
+
+        search_terms = all_search_terms[:1]
+        target_tasks = await self.listen_requests(search_terms)
+        self.tasks = self.tasks + target_tasks
+
+        def callback(future):
+            self.task_completion_times[future] = datetime.now()
+        for task in self.tasks:
+            task.add_done_callback(callback)
+
+        await self.await_pending_tasks()
+        try:
+            await self.check_completition_times(target_tasks)
+        except Exception as e:
+            assert e.__str__()==self.success_non_blocking
 
 
-@pytest.mark.skip(reason='not implemented')
-def test_handle_301_moved():
-    assert 1 == 2
+    @pytest.mark.skip(reason='not implemented')
+    def test_catches_404_page_not_found():
+        assert 1 == 2
 
 
-@pytest.mark.skip(reason='not implemented')
-def test_log_on_exception():
-    assert 1 == 2
+    @pytest.mark.skip(reason='not implemented')
+    def test_catches_400_bad_request():
+        assert 1 == 2
+
+
+    @pytest.mark.skip(reason='not implemented')
+    def test_handle_302_redirect():
+        assert 1 == 2
+
+
+    @pytest.mark.skip(reason='not implemented')
+    def test_handle_301_moved():
+        assert 1 == 2
+
+
+    @pytest.mark.skip(reason='not implemented')
+    def test_log_on_exception():
+        assert 1 == 2
