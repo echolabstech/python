4e25034383c4de57882574005c1b858d55da3c06
major <echolabstech@gmail.com>
2019-09-22 22:46:40 +0800

    added more test case failing tests

--- async/test_my_first_async.py
+++ async/test_my_first_async.py
@@ -8,6 +8,8 @@ def test_producer_non_blocking():
     assert 1 == 2
 
 
+
+@pytest.mark.skip(reason='not implemented')
 def test_producer_concurrent():
     """
     Producer B does work while Producer A awaits a result.
@@ -15,6 +17,7 @@ def test_producer_concurrent():
     assert 1 == 2
 
 
+@pytest.mark.skip(reason='not implemented')
 def test_consumer_non_blocking():
     """
     consumer A is awaiting a result, and passes control to the event loop.
@@ -22,8 +25,34 @@ def test_consumer_non_blocking():
     assert 1 == 2
 
 
+@pytest.mark.skip(reason='not implemented')
 def test_consumer_concurrent():
     """
     consumer B does work while Producer A awaits a result.
     """
+    assert 1 == 2
+
+
+@pytest.mark.skip(reason='not implemented')
+def test_catches_404_page_not_found():
+    assert 1 == 2
+
+
+@pytest.mark.skip(reason='not implemented')
+def test_catches_400_bad_request():
+    assert 1 == 2
+
+
+@pytest.mark.skip(reason='not implemented')
+def test_handle_302_redirect():
+    assert 1 == 2
+
+
+@pytest.mark.skip(reason='not implemented')
+def test_handle_301_moved():
+    assert 1 == 2
+
+
+@pytest.mark.skip(reason='not implemented')
+def test_log_on_exception():
     assert 1 == 2
