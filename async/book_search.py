24fe10b7761dea430d55dde441af02cdde361212
major <echolabstech@gmail.com>
2019-10-02 15:48:40 +0800

    cleanup routes

--- async/book_search.py
+++ async/book_search.py
@@ -4,21 +4,15 @@ import my_first_async
 routes = web.RouteTableDef()
 
 @routes.get('/')
-@routes.get('/{name}')
-async def handle(request):
-    name = request.match_info.get('name', 'Anonymous')
-    text = 'Hello, ' + name
-    return web.Response(text=text)
-
-@routes.get('/search/')
-@routes.get('/search/{search_terms}')
+@routes.get('/{search_terms}')
+@routes.get('/{search_terms}/')
 async def search(request):
     search_terms = []
     search_terms.append(request.match_info.get('search_terms', 'star wars'))
     url = 'http://localhost:8001/'
     tasks = await my_first_async.main(search_terms, url=url)
     handle_result = tasks.pop()
-    url, search_results = await handle_result # this blocks requests exponentially
+    url, search_results = await handle_result
     return web.Response(text=search_results)
 
 app = web.Application()
