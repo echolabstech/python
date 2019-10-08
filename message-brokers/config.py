fdec3a8158ae5c0d8dc0d785359f3591610cc902
major <echolabstech@gmail.com>
2019-10-08 18:39:09 +0800

    update config.py to use new lowercase naming convention

--- message-brokers/config.py
+++ message-brokers/config.py
@@ -1,3 +1,2 @@
-CELERY_RESULT_BACKEND='database'
-CELERY_RESULT_DBURI='sqlite:///temp.db'
-BROKER_URL='amqp://major:password@127.0.0.1:5672/major_host'
+result_backend='sqlite:///temp.db'
+broker_url='amqp://major:password@127.0.0.1:5672/major_host'
