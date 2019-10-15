a62ef98cc6a96a9e8650c099017920a97f9eba31
major <echolabstech@gmail.com>
2019-10-15 16:52:53 +0800

    tests for polls:index view

--- message-brokers/webapp/polls/tests.py
+++ message-brokers/webapp/polls/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.utils import timezone
+from django.db.models.query import QuerySet
+from django.urls import reverse
 
 from .models import Question
 
@@ -34,3 +36,84 @@ class QuestionModelTests(TestCase):
 		time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
 		recent_question = Question(pub_date=time)
 		self.assertIs(recent_question.was_published_recently(), True)
+
+class QuestionIndexViewTests(TestCase):
+
+	def create_question(self, question_text, days):
+		"""
+		Create a question with the given `question_text` and published the
+		given number of `days` offset to now (negative for questions published
+		in the past, positive for questions that have yet to be published).
+		"""
+		time = timezone.now() + datetime.timedelta(days=days)
+		return Question.objects.create(question_text=question_text, pub_date=time)
+
+	def test_returns_only_published_questions(self):
+		"""
+		returns published questions - witholding questions not yet published.
+		"""
+		recent_question = self.create_question(question_text='recent', days=-1)
+		future_question = self.create_question(question_text='future', days=30)
+		response = self.client.get(reverse('polls:index'))
+		
+		num_questions = len(response.context['latest_question_list'])
+		self.assertIs(num_questions, 1)
+
+		expected_question = response.context['latest_question_list'][0]
+		self.assertEqual(expected_question.question_text, recent_question.question_text)
+
+	def test_future_question(self):
+		"""
+		Questions with a pub_date in the future aren't displayed on
+		the index page.
+		"""
+		self.create_question(question_text="Future question.", days=30)
+		response = self.client.get(reverse('polls:index'))
+		self.assertContains(response, "No polls are available.")
+		self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+	def test_past_question(self):
+		"""
+		Questions with a pub_date in the past are displayed on the
+		index page.
+		"""
+		self.create_question(question_text="Past question.", days=-30)
+		response = self.client.get(reverse('polls:index'))
+		self.assertQuerysetEqual(
+			response.context['latest_question_list'],
+			['<Question: Past question.>']
+		)
+
+	def test_future_question_and_past_question(self):
+		"""
+		Even if both past and future questions exist, only past questions
+		are displayed.
+		"""
+		self.create_question(question_text="Past question.", days=-30)
+		self.create_question(question_text="Future question.", days=30)
+		response = self.client.get(reverse('polls:index'))
+		self.assertQuerysetEqual(
+			response.context['latest_question_list'],
+			['<Question: Past question.>']
+		)
+
+	def test_two_past_questions(self):
+		"""
+		The questions index page may display multiple questions.
+		"""
+		self.create_question(question_text="Past question 1.", days=-30)
+		self.create_question(question_text="Past question 2.", days=-5)
+		response = self.client.get(reverse('polls:index'))
+		self.assertQuerysetEqual(
+			response.context['latest_question_list'],
+			['<Question: Past question 2.>', '<Question: Past question 1.>']
+		)
+
+	def test_no_questions(self):
+		"""
+		If no questions exist, an appropriate message is displayed.
+		"""
+		response = self.client.get(reverse('polls:index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "No polls are available.")
+		self.assertQuerysetEqual(response.context['latest_question_list'], [])
--- message-brokers/webapp/polls/views.py
+++ message-brokers/webapp/polls/views.py
@@ -6,13 +6,17 @@ from django.views import generic
 
 from .models import Choice, Question
 
+from django.utils import timezone
+import datetime
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five questions published within the last day """
+        return Question.objects.filter(pub_date__lte=timezone.now()
+                                ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
